666c40535b6f46e26f0e715db47cb19d67d0b3cb
Jmishra49 <mishrajyotiraditya49@gmail.com>
2025-07-23 16:04:37 +0530

    Remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Preview from "./feature/preview/preview";
-import BlurPreview from "./feature/preview/BlurPreview";
+
 import MeetingPage from "./feature/preview/MeetingPage";
 import Feedback from "./feature/preview/Feedback";
 import { ZoomProvider } from "./feature/preview/ZoomContext";
@@ -13,7 +13,6 @@ const App = () => {
     <ZoomProvider>
       <Routes>
         <Route path="/" element={<Preview />} />
-        <Route path="/blur" element={<BlurPreview />} />
         <Route path="/meeting" element={<MeetingPage />} />
         <Route path="/feedback" element={<Feedback />} />
       </Routes>
